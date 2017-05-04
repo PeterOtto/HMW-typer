75c42fcc467430e052e5d2e80ecd7c9524ae7899
PeterOK <MUCpeku@Peters-MacBook-Pro.local>
2017-05-04 15:32:33 +0530

    preparing the typer for getting add to squarespace

--- 0.1/strings.js
+++ 0.1/strings.js
@@ -1,4 +1,31 @@
-var result = [
+var visibilityTyper = [
+'<h4 class="yellow">improve</h4> the tool from reporting to <h4 class="yellow">monitor</h4> the project <h4 class="yellow">in real time?</h4>',
+'increase the <h4 class="yellow">quality of data</h4> and make input easier?',
+'guarantee <h4 class="yellow">visibility up and downstream</h4> through the tool?',
+'increase the <h4 class="yellow">importance of governance</h4> in projects?',
+'create a tool <h4 class="yellow">attractive</h4>, supporting and <h4 class="yellow">easy to use?</h4>',
+'assure that <h4 class="yellow">internal projects</h4> are <h4 class="yellow">valued</h4> as much as external ones?'
+];
+
+var userTyper = [
+'<h4 class="yellow">improve</h4> the tool from reporting to <h4 class="yellow">monitor</h4> the project <h4 class="yellow">in real time?</h4>',
+'increase the <h4 class="yellow">quality of data</h4> and make input easier?',
+'guarantee <h4 class="yellow">visibility up and downstream</h4> through the tool?',
+'increase the <h4 class="yellow">importance of governance</h4> in projects?',
+'create a tool <h4 class="yellow">attractive</h4>, supporting and <h4 class="yellow">easy to use?</h4>',
+'assure that <h4 class="yellow">internal projects</h4> are <h4 class="yellow">valued</h4> as much as external ones?'
+];
+
+var governanceTyper = [
+'<h4 class="yellow">improve</h4> the tool from reporting to <h4 class="yellow">monitor</h4> the project <h4 class="yellow">in real time?</h4>',
+'increase the <h4 class="yellow">quality of data</h4> and make input easier?',
+'guarantee <h4 class="yellow">visibility up and downstream</h4> through the tool?',
+'increase the <h4 class="yellow">importance of governance</h4> in projects?',
+'create a tool <h4 class="yellow">attractive</h4>, supporting and <h4 class="yellow">easy to use?</h4>',
+'assure that <h4 class="yellow">internal projects</h4> are <h4 class="yellow">valued</h4> as much as external ones?'
+];
+
+var knowledgeTyper = [
 '<h4 class="yellow">improve</h4> the tool from reporting to <h4 class="yellow">monitor</h4> the project <h4 class="yellow">in real time?</h4>',
 'increase the <h4 class="yellow">quality of data</h4> and make input easier?',
 'guarantee <h4 class="yellow">visibility up and downstream</h4> through the tool?',
