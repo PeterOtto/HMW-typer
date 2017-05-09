b720e86ddb9990ff152c799af96cd4be6917c4f5
PeterOK <MUCpeku@Peters-MacBook-Pro.local>
2017-05-09 14:29:36 +0200

    changed the stings with the actual ideas.

--- 0.1/strings.js
+++ 0.1/strings.js
@@ -1,37 +1,53 @@
 var visibilityTyper = [
-'<h4 class="yellow">Improve</h4> the tool from reporting to <h4 class="yellow">monitor</h4> the project <h4 class="yellow">in real time.</h4>',
-'Increase the <h4 class="yellow">quality of data</h4> and make input easier.',
-'Guarantee <h4 class="yellow">visibility up and downstream</h4> through the tool.',
-'Increase the <h4 class="yellow">importance of governance</h4> in projects.',
-'Create a tool <h4 class="yellow">attractive</h4>, supporting and <h4 class="yellow">easy to use.</h4>',
-'Assure that <h4 class="yellow">internal projects</h4> are <h4 class="yellow">valued</h4> as much as external ones.'
+'Role based access to different stakeholders about critical information in the project, through individual dashboards',
+'Team directory with method of contact for each team member',
+'Control to Project & Delivery manager to share information at the right time, with relevant people (like Facebook post publicly or with selected people)',
+'Make provisions to identify projects with sensitive confidentiality issues (like NDA) requirements during setup',
+'Allow team members to report risks related to assigned tasks or delivery completion through the tool',
+'Allow the project team to see the advancement in other tasks and their global impact in the project',
+'Digitise critical elements (like SOW, contract commitments, etc.) for automatically setting up project planning and aspects in the project plan',
+'Sharing vision of the project at regular intervals to keep end goal in focus for example, making the screen savers specific to projects',
+'Sharing the impact of the deliverables of the project on the customer.'
 ];
 
 var userTyper = [
-'<h4 class="yellow">Improve</h4> the tool from reporting to <h4 class="yellow">monitor</h4> the project <h4 class="yellow">in real time.</h4>',
-'Increase the <h4 class="yellow">quality of data</h4> and make input easier.',
-'Guarantee <h4 class="yellow">visibility up and downstream</h4> through the tool.',
-'Increase the <h4 class="yellow">importance of governance</h4> in projects.',
-'Create a tool <h4 class="yellow">attractive</h4>, supporting and <h4 class="yellow">easy to use.</h4>',
-'Assure that <h4 class="yellow">internal projects</h4> are <h4 class="yellow">valued</h4> as much as external ones.'
-];
-
-
-var governanceTyper = [
-'<h4 class="yellow">Improve</h4> the tool from reporting to <h4 class="yellow">monitor</h4> the project <h4 class="yellow">in real time.</h4>',
-'Increase the <h4 class="yellow">quality of data</h4> and make input easier.',
-'Guarantee <h4 class="yellow">visibility up and downstream</h4> through the tool.',
-'Increase the <h4 class="yellow">importance of governance</h4> in projects.',
-'Create a tool <h4 class="yellow">attractive</h4>, supporting and <h4 class="yellow">easy to use.</h4>',
-'Assure that <h4 class="yellow">internal projects</h4> are <h4 class="yellow">valued</h4> as much as external ones.'
+'Make sandbox environment integral to the deployment process.',
+'Reinforce support system during Digi-Q roll out and focus on training modules and manuals.',
+'Create a platform where people can sign-up to be testers and try out the tool.',
+'Provide a chat bot in the tool that can resolve all kind of issues.',
+'Personalised push based tips & tricks which are relevant as per the phase of the project.',
+'Send SMS / warnings for needed inputs or problems.',
+'Make a seamless responsive tool to use across digital channels.',
+'Digitisation of online written documents directly into Digi-Q using text recognition from a picture of the document.',
+'Create a mentor/buddy system that enables users to get help in person.'
 ];
 
 
 var knowledgeTyper = [
-'<h4 class="yellow">Improve</h4> the tool from reporting to <h4 class="yellow">monitor</h4> the project <h4 class="yellow">in real time.</h4>',
-'Increase the <h4 class="yellow">quality of data</h4> and make input easier.',
-'Guarantee <h4 class="yellow">visibility up and downstream</h4> through the tool.',
-'Increase the <h4 class="yellow">importance of governance</h4> in projects.',
-'Create a tool <h4 class="yellow">attractive</h4>, supporting and <h4 class="yellow">easy to use.</h4>',
-'Assure that <h4 class="yellow">internal projects</h4> are <h4 class="yellow">valued</h4> as much as external ones.'
+'Automated push from veloci-Q and knowledge management to Digi-Q for quick access and easy references (pipeline)',
+'Relevant filters built into the tool for accessing correct information quickly. Enable prompt search through a bot',
+'The project team should need to post one challenge, observation or learning along with their timesheet submission each week',
+'Build a Knowledge management app to access and fetch knowledge anywhere',
+'Pre-filled texts in templates for information capture, so it’s easy to fill quality information',
+'Trigger notifications about recent best practice added to enable immediate sharing and consumption',
+'Get support of technical external teams to validate the quality of the content captured in learnings',
+'Voice & video record feature to capture tacit knowledge of the project (best practices, challenges, lessons learnt, constraints)',
+'Enable instant capture of learning through push notification, each time an event is triggered (task completed, milestone reached)'
+];
+
+
+var governanceTyper = [
+'Make review of project status directly from the tool rather than through presentations.',
+'Generate alarms, enabled through a bot, if documents are not updated in due time or if there has been unexpected behaviour like too much inactivity',
+'Create personalised reports and make them available for clients through crystal  (limited access) - immediate',
+'Digi-Q will record all data and information and feed it into customer tools as per customer requirements',
+'Automate report generation for different stakeholders which are customisable (by selecting sections) and that reflect the current state of the project',
+'Involve the PM in different customer reviews like MSR, QBR',
+'Automate predicting performance to enable course correction',
+'Auto addition of courses/skills developed during projects to each member (through My Profile)',
+'Provide flexibility within the tool to execute small/medium/large projects and different life-cycles',
+'Create a bridge that allows Digi-Q to read information from client tools as well as other Wipro tools',
+'Alert the Project Managers about direct consequences of their actions on the project and dependencies of their colleagues.',
+'Enable Digi-Q to generate a series of objects (data visualisation etc.), ready to use in the WSR template',
+'Establish mechanism to reward stakeholders and teams for up keeping of project management data'
 ];
